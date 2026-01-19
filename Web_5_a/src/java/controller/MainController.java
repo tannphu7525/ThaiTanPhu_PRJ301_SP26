93b70d5312a4ac60094985e9f756e3f690c8b6b3
tannphu7525 <thaiphu222@gmail.com>
2026-01-19 10:40:53 +0700

    check code

--- Web_5_a/src/java/controller/MainController.java
+++ Web_5_a/src/java/controller/MainController.java
@@ -18,15 +18,6 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class MainController extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String action = request.getParameter("action");
